0b9dcb35362f1c9d9ecfccb21e709534fa7c65ab
KyryloKalinichenko <Kirillstomper@gmail.com>
2021-10-22 15:40:11 +0200

    ok

--- cpp02/ex01/main.cpp
+++ cpp02/ex01/main.cpp
@@ -2,18 +2,28 @@
 #include "Fixed.hpp"
 
 int main( void ) {
-    Fixed a;
+    std::cout << "------------ CONSTRUCTORS ------------- " << std::endl;
+	Fixed a;
     Fixed const b( 10 );
     Fixed const c( 42.42f );
     Fixed const d( b );
     a = Fixed( 1234.4321f );
+
+    std::cout << "--------- FIXED POINT VALUES ---------- " << std::endl;
+
     std::cout << "a is " << a << std::endl;
     std::cout << "b is " << b << std::endl;
     std::cout << "c is " << c << std::endl;
     std::cout << "d is " << d << std::endl;
+
+    std::cout << "------------- INT VALUES -------------- " << std::endl;
+
     std::cout << "a is " << a.toInt() << " as integer" << std::endl;
     std::cout << "b is " << b.toInt() << " as integer" << std::endl;
     std::cout << "c is " << c.toInt() << " as integer" << std::endl;
     std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+
+    std::cout << "------------ DESTRUCTORS -------------- " << std::endl;
+
     return 0;
 }
