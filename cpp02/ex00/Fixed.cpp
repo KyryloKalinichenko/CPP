ff3b6e9945e3aa39d9d748a66cdb339f33046ee9
KyryloKalinichenko <Kirillstomper@gmail.com>
2021-10-25 09:22:47 +0200

    ok day 02

--- cpp02/ex00/Fixed.cpp
+++ cpp02/ex00/Fixed.cpp
@@ -1,34 +1,34 @@
 #include "Fixed.hpp"
 
-Fixed::Fixed( void ){
-    std::cout << "default constr" << std::endl;
+Fixed::Fixed( void ):_fixed(0){
+    std::cout << "Default constructor." << std::endl;
     return;
 }
 
 Fixed::Fixed(Fixed const & src){
-    std::cout << "copy constr" << std::endl;
+    std::cout << "Copy constructor." << std::endl;
     *this = src;
     return ;
 }
 
 Fixed::~Fixed( void ){
     // delete
-    std::cout << "default destr" << std::endl;
+    std::cout << "Default destructor." << std::endl;
     return;
 }
 
 int Fixed::getRawBits( void ){
-    std::cout << "get raw bits" << std::endl;
+    std::cout << "Get raw bits." << std::endl;
     return _fixed;
 }
 
 void Fixed::setRawBits(const int raw){
-    std::cout << "set raw bits" << std::endl;
+    std::cout << "Set raw bits." << std::endl;
     this->_fixed = raw;
 }
 
 Fixed & Fixed::operator=( Fixed const & rhs ){
-    std::cout << "oper" << std::endl;
+    std::cout << "Operator =" << std::endl;
     this->_fixed = rhs._fixed;
     // setRawBits(rhs.getRawBits());
     // ser var
--- cpp02/ex00/main.cpp
+++ cpp02/ex00/main.cpp
@@ -2,12 +2,27 @@
 #include "Fixed.hpp"
 
 int main( void ) {
+    std::cout << "------ CONSTRUCTORS ------" << std::endl;
+
     Fixed a;
     Fixed b( a );
     Fixed c;
     c = b;
+
+    std::cout << "--------- GETTERS --------" << std::endl;
     std::cout << a.getRawBits() << std::endl;
     std::cout << b.getRawBits() << std::endl;
     std::cout << c.getRawBits() << std::endl;
+
+    std::cout << "--------- SETTERS --------" << std::endl;
+	a.setRawBits(50);
+	b.setRawBits(50);
+	c.setRawBits(50);
+
+    std::cout << "--------- GETTERS --------" << std::endl;
+	std::cout << a.getRawBits() << std::endl;
+    std::cout << b.getRawBits() << std::endl;
+    std::cout << c.getRawBits() << std::endl;
+    std::cout << "------- DESTRUCTORS ------" << std::endl;
     return 0;
 }
